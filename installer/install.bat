9dd276e33c548e72de5335d8880328a4903786a6
olga-golovkina <72074265+olga-golovkina@users.noreply.github.com>
2020-09-30 13:09:06 +0300

    Delete install.bat

--- installer/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-copy LevelAlign.dll  %ProgramData%\Autodesk\Revit\Addins\2019\
-copy LevelAlign.addin %ProgramData%\Autodesk\Revit\Addins\2019\
-copy LevelAlign.dll  %AppData%\Autodesk\Revit\Addins\2019\
-copy LevelAlign.addin %AppData%\Autodesk\Revit\Addins\2019\
